12e710f2cff50905f99252e037b293eb0567d235
Cong-Quang <nguyencongquang02082003@gmail.com>
2024-03-19 07:30:36 +0700

    code functions demo

---
+++ snake.cpp
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+#ifdef _WIN32
+#include <windows.h>
+#else
+#include <unistd.h>
+#endif
+
+// Enum màu sắc
+typedef enum {
+    BLACK = 30,
+    RED = 31,
+    GREEN = 32,
+    YELLOW = 33,
+    BLUE = 34,
+    MAGENTA = 35,
+    CYAN = 36,
+    WHITE = 37
+} Color;
+
+void print(const char* text,int x, int y ,int color = 37){
+    char buffer[200];
+#ifdef _WIN32
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hConsole, color);
+    COORD pos = { x, y };
+    SetConsoleCursorPosition(pos,hConsole);
+    sprintf(buffer, "%s", text);
+    printf("%s", buffer);
+#else
+    sprintf(buffer, "\033[%d;%dH\033[%dm%s\033[0m", x, y, color, text);
+    printf("%s", buffer);
+#endif
+}
+
+// Hàm dừng màn hình trong milliseconds mili giây
+void pause(int milliseconds) {
+#ifdef _WIN32
+    Sleep(milliseconds);
+#else
+    usleep(milliseconds * 1000);
+#endif
+}
+
+int main() {
+
+    for (int i = 0; i < 10; i++)
+    {
+        print("quăng",i,i,i);
+    }
+    
+
+    return 0;
+}
